d9c39f9144f334e9f09d3803812b2b74549903f9
ss7622 <63588899+ss7622@users.noreply.github.com>
2024-02-12 13:59:33 +0900

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/AidietApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidietApplication.class, args);
+	}
+
+}
